948064d71734f586a79d1c2fd5e292c09dbe41b1
AmanVerma1167 <amanve0215@gmail.com>
2022-08-30 20:04:56 -0700

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,58 @@
+x=123676124
+y=-2456456
+z=478218815565678
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+x=1.10
+y=2.20
+z=.232
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
+
+name="bruce wayne 01"
+color='yellow'
+Institute='digipodium'
+print(name, type(name))
+print(color, type(color))
+print(Institute, type(Institute))
+
+is_coding=True
+is_fun=True
+are_u_bored=False
+print(is_coding, type(is_coding))
+print(is_fun, type(is_fun))
+print(are_u_bored, type(are_u_bored))
+
+x=None
+print(x, type(x))
+
+a=[5,10,15,20,25,30,35,40]
+vals=[1,2.2,'python']
+colors=['red','green','yellow']
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+a=(5,10,15,20,25,30,35,40)
+vals=(1,2.2,'python')
+colors=('red','green','yellow')
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+a={5,10,15,20,25,30,35,40}
+vals={1,2.2,'python'}
+colors={'red','green','yellow'}
+print(a, type(a))
+print(vals, type(vals))
+print(colors, type(colors))
+
+car_info={
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(car_info, type(car_info))
