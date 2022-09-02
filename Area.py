5a4b9b3fbf712ec11eac471b483bc43bb0016edd
AmanVerma1167 <amanve0215@gmail.com>
2022-09-03 01:23:47 +0530

    Update Area.py

--- Area.py
+++ Area.py
@@ -4,3 +4,9 @@ l = float(input("Enter the length:"))
 b = float(input("Enter the breadth:"))
 area= l * b
 print("Area of rectangle",area )
+
+# Area of circle
+
+r = float(input("Enter the radius:"))
+area = 2*3.14*r
+print("Area of circle",area)
