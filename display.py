9933751f2dddba1551c497a402da2191d12baf41
AmanVerma1167 <amanve0215@gmail.com>
2022-09-02 20:19:53 +0530

    Input

---
+++ display.py
@@ -0,0 +1,15 @@
+name = "Raj"
+age = 22
+
+print(name, age)
+print(name + str(age)) # typecast
+
+# my name is raj and i am 19 years old
+# comma seperation
+print("my name is", name, "and i am", age, "years old")
+print("my name is", name, "and i am", age, "years old", sep="😊") 
+
+# f-string (important)
+
+print(f"my name is {name} and i am {age} years old\n")
+print(f"my name is {name} \nand i am {age} years old")
---
+++ info.py
@@ -0,0 +1,15 @@
+# name = input("Enter your name:")
+# college = input("Where do you study:")
+# city = input("City name:")
+age = input("Enter your age:")
+# animal = input("National animal of INDIA:")
+
+# print(name,college,city,age,animal)
+
+age = int(age) # type casting
+print(age, type(age)) 
+
+a = int(input("Enter a number:"))
+b = int(input("Enter a number:"))
+
+print( a + b )
