c46972c888826cc44c50e16199e54dd35926bf7f
AmanVerma1167 <amanve0215@gmail.com>
2022-09-06 20:02:53 +0530

    loop(for)

---
+++ loop1.py
@@ -0,0 +1,7 @@
+names = ['Aman','Avinash','Sarika','Shruti','Jaya']
+for name in names:
+    print(name)
+
+msg = 'Journey before Destination'   
+for char in msg:
+    print(char)
---
+++ loop2.py
@@ -0,0 +1,42 @@
+# for i in range(100):
+#     print("Hello")
+
+# for i in range(100):
+#     print("Hello",i)   
+
+# for i in range(100):
+#     print(i, end=' ')
+
+# for i in range(1,24):
+#     print(i)  
+
+# for i in range(10,21):
+#     print(i, end='x')  
+
+# for i in range(1,11,2):
+#     print(i, end='0 ')   
+
+# reverse loop/
+# for i in range(10, 0, -1): 
+#     print(i, end=' ')   
+
+# print("=>")
+# data = [1,2,3,4,5,6,7,7,8,9]
+# for i,d in enumerate(data):
+#     print(i, d)
+
+# print("=>")
+# data = ['apple','cherry','guava','banana','samosa']
+# for i,d in enumerate(data):
+#     print(i, d)
+
+# print("=>")
+# data = ['apple','cherry','guava','banana','samosa']
+# for i in enumerate(data):
+#     print(i)    
+
+names = ['apple','banana','cherry']
+price = [100, 40, 65]
+
+for n,p in zip(names, price):
+    print(f'{n} => ${p}')
