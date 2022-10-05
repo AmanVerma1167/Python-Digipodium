8d54f5dd3c25e2ae09eab5456017573ac0ab1aa3
AmanVerma1167 <amanve0215@gmail.com>
2022-10-05 21:40:09 +0530

    Class And Object

---
+++ c1.py
@@ -0,0 +1,27 @@
+# class defination
+class Dog:
+    breed = 'Labrador'
+    age = 3
+    name = 'Tommy'
+
+# object instantiation
+dog1 = Dog()
+dog1.breed = 'German Shepherd'
+dog1.age = 5
+dog1.name = 'Sheru'
+
+dog2 = Dog()
+dog2.breed = 'Pug'
+dog2.age = 4
+dog2.name = 'Buddy'
+
+dog3 = Dog()
+dog3.breed = 'Golden Retrievar'
+dog3.age = 2
+dog3.name = 'Rex'
+
+print(dog1.breed, dog1.age, dog1.name)
+print(dog2.breed, dog2.age, dog2.name)
+print(dog3.breed, dog3.age, dog3.name)
+
+
